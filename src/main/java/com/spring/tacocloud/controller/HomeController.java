ca3f4e45c381e1430a3082a94976b8a2bebc93a1
cemÄilcakmak <b.cemilcakmak@gmail.com>
2021-12-22 23:26:47 +0300

    created first model

--- src/main/java/com/spring/tacocloud/controller/HomeController.java
+++ src/main/java/com/spring/tacocloud/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.spring.tacocloud.controllers;
+package com.spring.tacocloud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
