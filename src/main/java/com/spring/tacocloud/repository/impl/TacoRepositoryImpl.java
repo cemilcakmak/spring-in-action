23f179f200fc8ab733dd45a0a9266229169132b2
cemÄilcakmak <b.cemilcakmak@gmail.com>
2022-01-02 23:15:54 +0300

    made a general jpa implementation for repositories

--- src/main/java/com/spring/tacocloud/repository/impl/TacoRepositoryImpl.java
+++ src/main/java/com/spring/tacocloud/repository/impl/TacoRepositoryImpl.java
@@ -14,6 +14,7 @@ import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public class TacoRepositoryImpl implements TacoRepository {
@@ -60,4 +61,58 @@ public class TacoRepositoryImpl implements TacoRepository {
                 tacoId, ingredient.getId());
     }
 
+    @Override
+    public <S extends Taco> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Taco> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public Iterable<Taco> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<Taco> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Taco entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Taco> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
