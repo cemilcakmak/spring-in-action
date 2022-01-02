5d601e3222fe0e66d2bcbff6134ed7116fdfdf56
cemÄilcakmak <b.cemilcakmak@gmail.com>
2022-01-02 22:46:50 +0300

    turned repositories into jpa repo

--- src/main/java/com/spring/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/spring/tacocloud/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.spring.tacocloud.repository;
 
 import com.spring.tacocloud.domain.Order;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
     Order save(Order order);
 }
--- src/main/java/com/spring/tacocloud/repository/TacoRepository.java
+++ src/main/java/com/spring/tacocloud/repository/TacoRepository.java
@@ -1,8 +1,9 @@
 package com.spring.tacocloud.repository;
 
 import com.spring.tacocloud.domain.Taco;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TacoRepository {
+public interface TacoRepository extends CrudRepository<Taco, Long> {
 
     Taco save(Taco design);
 }
